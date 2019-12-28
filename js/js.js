b4fe913f03c3d97aa03e541ca2f8058a5467ee87
nice and good boy online <felix.ruttan@gmail.com>
2019-12-27 20:44:18 -0800

    Add files via upload

--- js/js.js
+++ js/js.js
@@ -17,6 +17,7 @@ function flexThis(itemToFlex){
 let contentForNav = {
 	
 	overview:'<div class="textContent" onclick="closeNav()">Prior to starting the program I received advice from [redacted until permission received] to learn about DevOps on my own time.' 
+	
 	+'<br><br>In addition I am stearing my education at BCIT towards endevors that will improve my DevOps/SRE knowledge whenever possible.</div>',
 	
 	fall19:'Level 1:'
@@ -25,19 +26,19 @@ let contentForNav = {
 	
 	+ '<div class="gallery-wrap">'
 	
-    + '<button class="item item-1" id="1420" onclick="flexThis(this)">	<span><h2>1420</h2>	</span>	<p id="1420Desc">An excellent primary with all topics relevent to my journey! It\'s not possible to cover every aspect of Systems Administration in 15 weeks, but every assignment and lecture contained key information. The instructor stressed that </p>	</button>'
+    + '<button class="item item-1" id="1420" onclick="flexThis(this)">	<span><h2>1420</h2>	</span>	<p id="1420Desc">	An excellent primary with all topics relevent to my journey! It\'s not possible to cover every aspect of Systems Administration in 15 weeks, but every assignment and lecture contained key information. The instructor stressed that </p>	</button>'
 	
-    + '<button class="item item-2" id="1515" onclick="flexThis(this)">	<span><h2>1515</h2></span>	<p id="1515Desc">text</p></button>'
+    + '<button class="item item-2" id="1515" onclick="flexThis(this)">	<span><h2>1515</h2></span>	<p id="1515Desc">	text</p></button>'
 	
-    + '<button class="item item-3" id="1630" onclick="flexThis(this)">	<span><h2>1630</h2></span>	<p id="1630Desc"></p></button>'
+    + '<button class="item item-3" id="1630" onclick="flexThis(this)">	<span><h2>1630</h2></span>	<p id="1630Desc">	</p></button>'
 	
-    + '<button class="item item-4" id="1620" onclick="flexThis(this)">	<span><h2>1620</h2><span>	<p id="1620Desc"></p></button>'
+    + '<button class="item item-4" id="1620" onclick="flexThis(this)">	<span><h2>1620</h2><span>	<p id="1620Desc">	</p></button>'
 	
-    + '<button class="item item-5" id="1310" onclick="flexThis(this)">	<span><h2>1310</h2></span>	<p id="1310Desc">The highlight of this course was the 90 minutes we spent discussing BigO notation! The rest of the material shed light on how computers operate on the metal and some fundamental algorithmic insights.</p></button>'
+    + '<button class="item item-5" id="1310" onclick="flexThis(this)">	<span><h2>1310</h2></span>	<p id="1310Desc">	The highlight of this course was the 90 minutes we spent discussing BigO notation! The rest of the material shed light on how computers operate on the metal and some fundamental algorithmic insights.</p></button>'
 	
-	+ '<button class="item item-6" id="1100" onclick="flexThis(this)">	<span><h2>1100</h2></span>	<p id="1100Desc">Somewhat difficult to parse the value of this course beyond additional experience working in teams. The majority of the material is aimed at business students who want to manage people. Did not align with DevOps or Agile principals which I regularly insistent were relavent and perhaps more important in the context of the CIT program as a whole.</p></button>'
+	+ '<button class="item item-6" id="1100" onclick="flexThis(this)">	<span><h2>1100</h2></span>	<p id="1100Desc">	Somewhat difficult to parse the value of this course beyond additional experience working in teams. The majority of the material is aimed at business students who want to manage people. Did not align with DevOps or Agile principals which I regularly insistent were relavent and perhaps more important in the context of the CIT program as a whole.</p></button>'
 	
-    + '<button class="item item-7" id="1116" onclick="flexThis(this)">	<span><h2>1116</h2></span>	<p id="1116Desc"></p></button>' + 	'</div></div>',
+    + '<button class="item item-7" id="1116" onclick="flexThis(this)">	<span><h2>1116</h2></span>	<p id="1116Desc">	</p></button>' + 	'</div></div>',
 	
 	winterBreak:'Personal Projects Winter 2019:<br>'
 	
@@ -52,26 +53,206 @@ let contentForNav = {
     + '<button class="item item-3" id="DogPetterPro" onclick="flexThis(this)">	<span><h2>DogPetterPro</h2></span>	<p id="DogPetterProDesc">Revisiting Dog Petter to force landscape, improve responsiveness, and optimize for web.</p></button>'
 	
 	+ '<button class="item item-4" id="dualBoot" onclick="flexThis(this)">	<span><h2>Dual Boot Hackintosh/Linux</h2></span>	<p id="dualBootDesc">text</p></button>',
+	
 	winter20:'Level 2',
+	
 	summerBreak:'Futre Event',
 	}
 	
 let navContent = document.getElementById('navContent');
 
 $(document).ready(function() {
-	$("h1").connections({ to: "div:first", class: "first" });
-	$("div.overview").connections({to: "div.fall19", class: "first"});
-	$("div.overview").connections({to: "div.personal"});
+	$("div.overview").connections({to: "div.fall19", class: "dashed"});
+	$("div.overview").connections({to: "div.personal", class:"dashed"});
 	
-	$("div.fall19").connections({to: "div.fall19prj"});
-	$("div.fall19").connections({to: "div.winterBreak"});
+		$("div.personal .anchor").connections({
+			to: "div.personal li:nth-child(1)",
+			within: ".personal",
+			tag: "inner",
+			css:{
+				borderStyle: 'ridge outset',
+				borderWidth: '.8vw',
+				borderBottomLeftRadius: 70,
+				borderColor: '#c6e0ef',
+			}
+			});$("div.personal .anchor").connections({
+			to: "div.personal li:nth-child(2)",
+			within: ".personal",
+			tag: "inner",
+			css:{
+				borderStyle: 'ridge outset',
+				borderWidth: '.8vw',
+				borderBottomLeftRadius: 70,
+				borderColor: '#c6e0ef',
+			}
+			});
+			$("div.personal .anchor").connections({
+			to: "div.personal li:nth-child(3)",
+			within: ".personal",
+			tag: "inner",
+			css:{
+				borderStyle: 'ridge outset',
+				borderWidth: '.8vw',
+				borderBottomLeftRadius: 70,
+				borderColor: '#c6e0ef',
+			}
+			});
+			
+			
 	
-	$("div.winterBreak").connections({to: "div.winterBreakprj"});
+	$("div.fall19").connections({to: "div.fall19prj", class:"timePeriod"});
 	
-	$("div.winterBreak").connections({to: "div.winter20"});
-	$("div.winter20").connections({to: "div.winter20prj"});
 	
-	$("div.winter20").connections({to: "div.summerBreak"});
+		$("div.js19 .anchor").connections({
+			to: "div.js19 li:nth-child(1)",
+			within: ".js19",
+			tag: "inner",
+			css:{
+				borderStyle: 'ridge outset',
+				borderWidth: '.8vw',
+				borderBottomLeftRadius: 70,
+				borderColor: '#c6e0ef',
+			}
+			});
+		$("div.js19 .anchor").connections({
+			to: "div.js19 li:nth-child(2)",
+			within: ".js19",
+			tag: "inner",
+			css:{
+				borderStyle: 'ridge outset',
+				borderWidth: '.8vw',
+				borderBottomLeftRadius: 70,
+				borderColor: '#c6e0ef',
+			}
+			});
+		$("div.js19 .anchor").connections({
+			to: "div.js19 li:nth-child(3)",
+			within: ".js19",
+			tag: "inner",
+			css:{
+				borderStyle: 'ridge outset',
+				borderWidth: '.8vw',
+				borderBottomLeftRadius: 70,
+				borderColor: '#c6e0ef',
+			}
+			});
+		$("div.js19 .anchor").connections({
+			to: "div.js19 li:nth-child(4)",
+			within: ".js19",
+			tag: "inner",
+			css:{
+				borderStyle: 'ridge outset',
+				borderWidth: '.8vw',
+				borderBottomLeftRadius: 70,
+				borderColor: '#c6e0ef',
+			}
+			});
+	
+	
+	
+		$("div.db19 .anchor").connections({
+			to: "div.db19 li:nth-child(1)",
+			within: ".db19",
+			tag: "inner",
+			css:{
+				borderStyle: 'ridge outset',
+				borderWidth: '.8vw',
+				borderBottomLeftRadius: 70,
+				borderColor: '#c6e0ef',
+			}
+			});
+		$("div.db19 .anchor").connections({
+			to: "div.db19 li:nth-child(2)",
+			within: ".db19",
+			tag: "inner",
+			css:{
+				borderStyle: 'ridge outset',
+				borderWidth: '.8vw',
+				borderBottomLeftRadius: 70,
+				borderColor: '#c6e0ef',
+			}
+			});
+		$("div.db19 .anchor").connections({
+			to: "div.db19 li:nth-child(3)",
+			within: ".db19",
+			tag: "inner",
+			css:{
+				borderStyle: 'ridge outset',
+				borderWidth: '.8vw',
+				borderBottomLeftRadius: 70,
+				borderColor: '#c6e0ef',
+			}
+			});
+		$("div.db19 .anchor").connections({
+			to: "div.db19 li:nth-child(4)",
+			within: ".db19",
+			tag: "inner",
+			css:{
+				borderStyle: 'ridge outset',
+				borderWidth: '.8vw',
+				borderBottomLeftRadius: 70,
+				borderColor: '#c6e0ef',
+			}
+			});
+			
+			
+			
+		$("div.wr19 .anchor").connections({
+			to: "div.wr19 li:nth-child(1)",
+			within: ".wr19",
+			tag: "inner",
+			css:{
+				borderStyle: 'ridge outset',
+				borderWidth: '.8vw',
+				borderBottomLeftRadius: 70,
+				borderColor: '#c6e0ef',
+			}
+			});
+		$("div.wr19 .anchor").connections({
+			to: "div.wr19 li:nth-child(2)",
+			within: ".wr19",
+			tag: "inner",
+			css:{
+				borderStyle: 'ridge outset',
+				borderWidth: '.8vw',
+				borderBottomLeftRadius: 70,
+				borderColor: '#c6e0ef',
+			}
+			});
+		$("div.wr19 .anchor").connections({
+			to: "div.wr19 li:nth-child(3)",
+			within: ".wr19",
+			tag: "inner",
+			css:{
+				borderStyle: 'ridge outset',
+				borderWidth: '.8vw',
+				borderBottomLeftRadius: 70,
+				borderColor: '#c6e0ef',
+			}
+			});
+		$("div.wr19 .anchor").connections({
+			to: "div.wr19 li:nth-child(4)",
+			within: ".wr19",
+			tag: "inner",
+			css:{
+				borderStyle: 'ridge outset',
+				borderWidth: '.8vw',
+				borderBottomLeftRadius: 70,
+				borderColor: '#c6e0ef',
+			}
+			});
+		
+		
+		
+	$("div.fall19").connections({to: "div.winterBreak", class:"timePeriod"});
+	
+	$("div.winterBreak").connections({to: "div.winterBreakprj", class:"timePeriod"});
+	
+	$("div.winterBreak").connections({to: "div.winter20", class:"timePeriod"});
+	
+	$("div.winter20").connections({to: "div.winter20prj",class:"timePeriod"});
+	
+	$("div.winter20").connections({to: "div.summerBreak",class:"timePeriod"});
 	
 	$("connection:odd").addClass("odd");
 
